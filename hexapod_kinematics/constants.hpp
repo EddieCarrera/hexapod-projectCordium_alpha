bf4db50697bf0578d26b488d611bec60d8cb430f
EddieCarrera <Carrera318a@gmail.com>
2024-02-04 22:38:17 -0800

    Porting kinematics to CPP WIP

--- hexapod_kinematics/constants.hpp
+++ hexapod_kinematics/constants.hpp
@@ -1,24 +1,80 @@
-const char* LEG_POINT_TYPES_LIST[] = {
-    "bodyContactPoint",
-    "coxiaPoint",
-    "femurPoint",
-    "footTipPoint",
-}
+#pragma once
 
-const POSITION_NAME_TO_ID_MAP = {
-    rightMiddle: 0,
-    rightFront: 1,
-    leftFront: 2,
-    leftMiddle: 3,
-    leftBack: 4,
-    rightBack: 5,
-}
+#include <stdint.h>
+#include <string>
 
-const POSITION_NAMES_LIST = [
-    "rightMiddle",
-    "rightFront",
-    "leftFront",
-    "leftMiddle",
-    "leftBack",
-    "rightBack",
-]
+const std::string LEG_POINT_TYPES_LIST[] = {
+	"bodyContactPoint",
+	"coxiaPoint",
+	"femurPoint",
+	"footTipPoint",
+};
+
+enum POSITION_NAME_TO_ID_MAP {
+	rightMiddle,
+	rightFront,
+	leftFront,
+	leftMiddle,
+	leftBack,
+	rightBack
+};
+
+const std::string POSITION_NAMES_LIST[] = {
+	"rightMiddle",
+	"rightFront",
+	"leftFront",
+	"leftMiddle",
+	"leftBack",
+	"rightBack"
+};
+
+const std::string POSITION_NAMES_LIST[] = {
+	"alpha", 
+	"beta", 
+	"gamma"    
+};
+
+enum POSITION_NAME_TO_ID_MAP {
+	alpha = 90,
+	beta = 180,
+	gamma = 180
+};
+
+/*
+
+	hexapodYaxis
+			^
+			|
+			|
+			*-----> hexapodXaxis
+		/ (cog)
+		/
+hexapodZaxis
+
+Relative x-axis, for each attached linkage
+
+(+135)  x2          x1 (+45)
+					\   head  /
+					*---*---*
+					/    |    \
+				/     |     \
+(+180)  /      |      \
+	x3 --*------cog------*-- x0 (+0)
+				\      |      /
+				\     |     /
+					\    |    /
+					*---*---*
+					/         \
+				x4           x5
+		(+225)        (+315)
+*/
+enum POSITION_NAME_TO_AXIS_ANGLE_MAP {
+	rightMiddle = 0,
+	rightFront = 45,
+	leftFront = 135,
+	leftMiddle = 180,
+	leftBack = 225,
+	rightBack = 315
+};
+
+const uint8_t NUMBER_OF_LEGS = 6;
---
+++ hexapod_kinematics/vector.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+class Vector {
+
+	private:
+	/* Variables */
+	int32_t x;
+	int32_t y;
+	int32_t z;
+	std::string name;
+	std::string id;
+
+	public:
+
+
+	/* Constructor */
+	public:
+	Vector(int32_t x, int32_t y, int32_t z, std::string name, std::string id);
+
+	/* Methods */
+
+	public:
+	void newTrot()
+
+	private:
+
+
+};
