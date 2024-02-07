162c5ee81cfab0211a541c7c2fdd66da493dfd3a
Eddie Carrera <Carrera318a@gmail.com>
2024-02-07 00:24:56 +0000

    Porting vector library

--- hexapod_kinematics/vector.hpp
+++ hexapod_kinematics/vector.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <stdint.h>
+#include <string.h>
 #include <string>
 
 class Vector {
 
-	private:
 	/* Variables */
 	float x;
 	float y;
@@ -13,28 +13,21 @@ class Vector {
 	std::string name;
 	std::string id;
 
-	public:
-
-
 	/* Constructor */
 	public:
 	Vector(float x, float y, float z, std::string name, std::string id);
 
 	/* Methods */
-
 	public:
 	/**
 	 * Given point `point` location wrt a local axes coordinate frame
 	 * find point in a global axes coordinate frame where the local 
 	 * axes wrt the global frame is defined by parameter transformMatrix 
 	 * */
-	void newTrot(Vector *outputVector, float transformMatrix[4][4], std::string name, std::string id);
+	void newTrot(Vector *outputVector, float tranfMtrx[4][4], std::string name, std::string id);
 	
-	void cloneTrot(Vector *outputVector, float transformMatrix[4][4]);
-	void cloneShift(Vector *outputVector, float x, float y, float z);
-	void cloneTrotShift(Vector *outputVector, float transformMatrix[4][4], float x, float y, float z);
-
-	private:
-
-
+	void cloneTrot(Vector *outputVector, float tranfMtrx[4][4]);
+	void cloneShift(Vector *outputVector, float tx, float ty, float tz);
+	void cloneTrotShift(Vector *outputVector, float tranfMtrx[4][4], float tx, float ty, float tz);
+	void toMarkdownString(char *markdownString);
 };
